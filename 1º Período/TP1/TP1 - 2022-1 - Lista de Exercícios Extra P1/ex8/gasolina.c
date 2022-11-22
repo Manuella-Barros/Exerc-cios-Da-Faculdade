c4468678cb7048e2df5a3f33ebe66b6b6c8ff1fe
Manuella Barros <manubarrossa@gmail.com>
2022-11-21 23:41:31 -0300

    ex

--- 1º Período/TP1/TP1 - 2022-1 - Lista de Exercícios Extra P1/ex8/gasolina.c
+++ 1º Período/TP1/TP1 - 2022-1 - Lista de Exercícios Extra P1/ex8/gasolina.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 
+int calculoValor(float litrosVendidos, char combustivel);
+
 int main(void){
-    
+    float litrosVendidos;
+    char combustivel;
+
+    printf("Insira quantos litros foram vendidos e qual a gasolinha => ");
+    scanf("%f %s", &litrosVendidos, &combustivel); 
+    calculoValor(litrosVendidos, combustivel);
+}
+
+int calculoValor(float litrosVendidos, char combustivel){
+    float custo;
+
+    //CALCULANDO O PREÇO SEM DESCONTO
+    if(combustivel == 'g' || combustivel == 'G'){    
+        //CALCULANDO O DESCONTO
+        if(litrosVendidos <= 20){
+            custo = litrosVendidos * 3.168; //desconto de 4% por litro
+        } else{
+            custo = litrosVendidos * 3.102; //desconto de 6% por litro
+        }
+
+    } else if(combustivel == 'a' || combustivel == 'A'){
+        //CALCULANDO O DESCONTO
+        if(litrosVendidos <= 20){
+            custo = litrosVendidos * 2.813; //desconto de 3% por litro
+        } else{
+            printf("foi");
+            custo = litrosVendidos * 2.755; //desconto de 5% por litro
+        }
+    }
+
+    printf("Custo = %.2f", custo);
 }
---
+++ 1º Período/TP1/TP1 - 2022-1 - Lista de Exercícios Extra P1/ex9/notas.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(void){
+    float p1, p2, p2ch, media, pf;
+
+    printf("Insira a nota da p1 e p2 do aluno, se ele faltou digite -1 => ");
+    scanf("%f", &p1);
+    scanf("%f", &p2);
+
+    if(p1 != -1 && p2 != -1){ //calculo para se n faltou nenhuma
+        media = (p1 + p2) / 2; 
+    
+    } else if(p1 == -1 && p2 != -1){ //calculo para segunda chamada da p1
+        printf("Insira a nota da segunda chamada da p1 desse aluno ");
+        scanf("%f", &p2ch);
+        
+        if(p2ch != -1){ //calculo para caso tenha faltado a segunda chamada da p1
+            media = (p2 + p2ch) / 2;
+        } else{
+            media = p2 / 2;
+        }
+        
+    } else if(p2 == -1 && p1 != -1){ //calculo para segunda chamada da p2
+        printf("Insira a nota da segunda chamada da p2 desse aluno ");
+        scanf("%f", &p2ch);
+
+        if(p2ch != -1){ //calculo para caso tenha faltado a segunda chamada da p2
+            media = (p1 + p2ch) / 2;
+        } else{
+            media = p1 / 2;
+        }
+        
+    } else if(p2 == -1 && p1 == -1){ // calculo caso tenha faltado as 2 provas
+        printf("Insira a nota da segunda chamada desse aluno ");
+        scanf("%f", &p2ch);
+
+        if(p2ch != -1){
+            media = p2ch / 2;    
+        } else{
+            media = 0; // calculo caso tenha faltado tudo
+        }
+    }
+
+    // pra ver se passou ou nao
+    if(media >= 7){
+        printf("Aprovado");
+    } else if(media < 4){
+        printf("Reprovado");
+    } else{
+        printf("Insira a nota da pf desse aluno ");
+        scanf("%f", &pf);
+        media = (media + pf) / 2;
+
+        if(media == -1){
+            media = media / 2;
+        }
+
+        if(media >= 5){
+            printf("Aprovado");
+        } else{
+            printf("Reprovado");
+        }
+    }
+}
