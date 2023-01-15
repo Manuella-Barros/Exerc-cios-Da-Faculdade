aefb0c4652fcf29740401a7caf27beff437f2aaf
Manuella Barros <manubarrossa@gmail.com>
2023-01-15 19:04:41 -0300

    Primeiro exercicio de struct

---
+++ 1º Período/TP1/Exercicios Slide/Slide 10 - Struct/ex1/index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct dados_pessoas {// aqui eu crio a struct com o nome de dados_pessoas
+    char nome[100];
+    float peso;
+    float altura;
+} pessoas; //pessoas é o tipo q eu dei pra ela, um tipo como char, int e float
+
+int leDados(pessoas p, int contador);
+
+int main(void){
+    pessoas p[5];
+
+    for(int i = 0; i < 5; i++){
+        printf("\n\nInsira os dados do funcionario %i: \n", i+1);
+
+        printf("Nome: ");
+        scanf("%s", &p[i].nome); 
+        printf("Peso: ");
+        scanf("%f", &p[i].peso);
+        printf("Altura: ");
+        scanf("%f", &p[i].altura);
+    }
+
+    for(int n = 0; n < 5; n++){
+        printf("\n\nNome: %s\n", p[n].nome);
+        printf("IMC: %.2f\n", p[n].peso / (p[n].altura * p[n].altura));//calculo imc
+    }
+}
