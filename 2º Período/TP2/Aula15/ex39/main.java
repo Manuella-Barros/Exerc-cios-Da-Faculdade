9a8451cd2c1e6eb7fe2f001a7e9b5384e2deb231
Manuella Barros <manubarrossa@gmail.com>
2023-05-15 11:09:05 -0300

    Exercicios de coleção Java

--- 2º Período/TP2/Aula15/ex39/main.java
+++ 2º Período/TP2/Aula15/ex39/main.java
@@ -16,15 +16,21 @@ public class main {
 
         for(int i = 0; i < palavra.length(); i++){ //Le cada letra da palavra
             for (int j = 0; j < 26; j++){ //Le cada letra do alfabeto
-                if(!listaLetras.contains(alfabeto.charAt(j))){
+                if(!listaLetras.contains(alfabeto.charAt(j))){ //se a lista letra n tiver a letra alfabeto[i]
                     listaLetras.add(alfabeto.charAt(j)); //aqui eu to criando uma lista com tds as letras do alfabeto
-                    
+                    listaQuantidade.add(0);// preenche a lista de quantidade com 0;
                 }
-                //if(palavra.charAt(i) == alfabeto.charAt(j)){
-                //    System.out.println(palavra.charAt(i) + " é igual a " + alfabeto.charAt(j));
-                //}
+                if(palavra.charAt(i) == alfabeto.charAt(j)){
+                    System.out.println(palavra.charAt(i) + " é igual a " + alfabeto.charAt(j));
+                    //int num = (int)listaQuantidade.get(j) + 1;
+                    listaQuantidade.set(j, (int)listaQuantidade.get(j) + 1);
+                }
             }
         }
-        System.out.println(listaQuantidade);
+
+        System.out.println("A palavra tem: ");
+        for (int i = 0; i < alfabeto.length(); i++){
+            System.out.println("> " + listaQuantidade.get(i) + " letras " + listaLetras.get(i));
+        }
     }
 }
